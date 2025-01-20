53f358f6b80350a19ee40ef2ffebdb92288ad6d7
starpolar <105044555+starpolar@users.noreply.github.com>
2025-01-20 17:53:59 +0900

    [Bronze II] Title: 완전제곱수, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Bronze/1977. 완전제곱수/완전제곱수.c
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.c
@@ -1,22 +1,21 @@
 // 1977 완전제곱수 / 1.20
 #include <stdio.h>
-#include <math.h>
 
 int main() {
 
 	int m, n;
 	scanf("%d %d", &m, &n);
 
-	int num=1, pow, sum = 0, min = 10001;
-	//if (m != 1) num = (int)sqrt(m) + 1;
+	int num=1,pow,sum=0, min=10001;
 	while (1) {
 		pow = num * num;
+        num++;
+		if (pow < m) continue;
 		if (n < pow) break;
-		else if(m <= pow) {
+		else {
 			sum += pow;
 			if (min > pow) min = pow;
 		}
-		num++;
 	}
 
 	if (sum == 0) printf("-1");
