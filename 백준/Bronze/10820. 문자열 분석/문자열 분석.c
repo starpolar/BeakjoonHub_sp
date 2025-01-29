5be15750d40e3b080795955b44fbca06bc35f131
starpolar <105044555+starpolar@users.noreply.github.com>
2025-01-29 23:23:44 +0900

    [Bronze II] Title: 문자열 분석, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.c
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 
 int main() {
-    char str[1010];  // 최대 길이가 1000이므로 충분한 크기로 선언
+    char str[102];  // 최대 길이가 1000이므로 충분한 크기로 선언
 
     while (fgets(str, sizeof(str), stdin)) {  // 입력이 존재할 때까지 반복
         int lower = 0, upper = 0, digit = 0, space = 0;
