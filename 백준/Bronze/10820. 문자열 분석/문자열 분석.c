c1f7ab5fda0102822dc14be1da8fa984f1866a92
starpolar <105044555+starpolar@users.noreply.github.com>
2025-01-29 23:20:49 +0900

    [Bronze II] Title: 문자열 분석, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[1010];  // 최대 길이가 1000이므로 충분한 크기로 선언
+
+    while (fgets(str, sizeof(str), stdin)) {  // 입력이 존재할 때까지 반복
+        int lower = 0, upper = 0, digit = 0, space = 0;
+
+        for (int i = 0; str[i] != '\0'; i++) {
+            if (islower(str[i])) lower++;
+            else if (isupper(str[i])) upper++;
+            else if (isdigit(str[i])) digit++;
+            else if (str[i] == ' ') space++;
+        }
+
+        printf("%d %d %d %d\n", lower, upper, digit, space);
+    }
+
+    return 0;
+}
